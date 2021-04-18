8b9ea9748ad9623726b1e4da95f2a3cd12764c85
kamil.korczak <kamil.korczak@gmail.com>
2021-04-18 22:40:57 +0200

    Health controller

---
+++ MedicineRegistry.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicineRegistry.Api.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class HealthController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult Get()
+    {
+      return Ok();
+    }
+  }
+}
